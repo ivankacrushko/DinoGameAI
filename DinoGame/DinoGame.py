3c3bdbe0e97dfed57c7197678e909e0357ff44d3
ivankacrushko <103641183+ivankacrushko@users.noreply.github.com>
2024-01-17 13:59:46 +0100

    1.1.1
    start screen, death screen, death count, changed to polish language

--- DinoGame/DinoGame.py
+++ DinoGame/DinoGame.py
@@ -21,6 +21,7 @@ screen = pygame.display.set_mode((screenW, screenH))
 
 
 highest_score = 0
+death_count = 0
 
 file_path = 'train_data.txt'
 loaded_data_str = np.loadtxt(file_path, dtype=str)
@@ -371,7 +372,7 @@ gen1.train(np.transpose(train_X), np.transpose(train_Y), NN_ARCHITECTURE, 1000, 
 
 
 def main():
-    global model, game_speed, x_pos_bg, y_pos_bg,highest_score, points,obstacles, obstacle_distance, obstacle_width, obstacle_height
+    global model, game_speed, x_pos_bg, y_pos_bg,highest_score, points,obstacles, obstacle_distance, obstacle_width, obstacle_height, death_count
     run = True
     clock = pygame.time.Clock()
     player = Dinosaur()    
@@ -383,13 +384,45 @@ def main():
     obstacles = []
     font = pygame.font.Font('freesansbold.ttf', 20)
     
+    
     obstacle_distance = 0
     obstacle_width = 0
     obstacle_height = 0
     is_flying = -1
     
-    ##############
+    def score(highest_score, death_count):
+        global points, game_speed
+        points += 1
+        if points % 100 == 0:
+            game_speed +=1            
+        text = font.render("Punkty: " + str(points), True, (0,0,0))
+        textRect = text.get_rect()
+        textRect.center = (1000,40)
+        screen.blit(text, textRect)
+
+        text1 = font.render("Rekord: " + str(highest_score), True, (0,0,0))
+        textRect = text1.get_rect()
+        textRect.center = (1000,60)
+        screen.blit(text1, textRect)
         
+        text2 = font.render("Smierci: " + str(death_count), True, (0,0,0))
+        textRect = text2.get_rect()
+        textRect.center = (1000,80)
+        screen.blit(text2, textRect)
+
+    def backgroundDraw():
+        global x_pos_bg, y_pos_bg
+
+        image_width = background.get_width()
+        screen.blit(background, (x_pos_bg, y_pos_bg))
+        screen.blit(background, (image_width + x_pos_bg, y_pos_bg))
+
+        if x_pos_bg <= -image_width:
+            screen.blit(background, (image_width + x_pos_bg, y_pos_bg))
+            x_pos_bg = 0
+        x_pos_bg -= game_speed    
+    
+
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -424,7 +457,8 @@ def main():
             obstacle.draw(screen)
             obstacle.update()
             if player.dino_rect.colliderect(obstacle.rect):
-                menu()
+                death_count += 1
+                menu(death_count)
 
         try:
             obstacle_distance = obstacles[-1].rect.x
@@ -434,70 +468,44 @@ def main():
             pass
 
         if points > highest_score:
-            highest_score = points
-
-        def score(highest_score):
-            global points, game_speed
-            points += 1
-            if points % 100 == 0:
-                game_speed +=1            
-            text = font.render("Points: " + str(points), True, (0,0,0))
-            textRect = text.get_rect()
-            textRect.center = (1000,40)
-            screen.blit(text, textRect)
-
-            text1 = font.render("Highest Score: " + str(highest_score), True, (0,0,0))
-            textRect = text1.get_rect()
-            textRect.center = (1000,60)
-            screen.blit(text1, textRect)
-
-        def backgroundDraw():
-            global x_pos_bg, y_pos_bg
-
-            image_width = background.get_width()
-            screen.blit(background, (x_pos_bg, y_pos_bg))
-            screen.blit(background, (image_width + x_pos_bg, y_pos_bg))
-
-            if x_pos_bg <= -image_width:
-                screen.blit(background, (image_width + x_pos_bg, y_pos_bg))
-                x_pos_bg = 0
-            x_pos_bg -= game_speed
+            highest_score = points    
         
         backgroundDraw()
         cloud.draw(screen)
         cloud.update()
-        score(highest_score)        
+        score(highest_score, death_count)        
         clock.tick(30)
         pygame.display.update()
 
-        def menu():
-            global points
-            run = True
+def menu(death_count):
+    global points
+    run = True
             
-            while run:
-                screen.fill((255,255,255))
-                font = pygame.font.Font('freesansbold.ttf', 20)
-
-                text = font.render("Press any key to start", True, (0,0,0))
-                
-                
-                text = font.render("Press any key to restart", True, (0,0,0))
-                score = font.render("Your score: " +str(points), True, (0,0,0))
-                scoreRect = score.get_rect()
-                scoreRect.center = (screenW //2, screenH // 2 + 50)
-                screen.blit(score, scoreRect)
-                textRect = text.get_rect()
-                textRect.center = (screenW // 2, screenH // 2)
-                screen.blit(text, textRect)
-                pygame.display.update()   
-
+    while run:
+        screen.fill((255,255,255))
+        font = pygame.font.Font('freesansbold.ttf', 20)
+        if death_count == 0:
+            text = font.render("Nacisnij dowolny klawisz aby zaczac", True, (0,0,0))
+        elif death_count > 0:
+            text = font.render("Nacisnij dowolny klawisz aby ponowic", True, (0,0,0))
+            score = font.render("Zdobyto punktow: " +str(points), True, (0,0,0))
+            deaths = font.render("Podejscie: " +str(death_count), True, (0,0,0))
+            scoreRect = score.get_rect()
+            scoreRect.center = (screenW //2, screenH // 2 + 50)
+            screen.blit(score, scoreRect)
+            deathRect = deaths.get_rect()
+            deathRect.center = (screenW //2, screenH // 2 + 70)
+            screen.blit(deaths, deathRect)
+        textRect = text.get_rect()
+        textRect.center = (screenW // 2, screenH // 2)
+        screen.blit(text, textRect)
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type ==pygame.QUIT:
+                run = False
+            if event.type == pygame.KEYDOWN:
                 main()
-                for event in pygame.event.get():
-                    if event.type ==pygame.QUIT:
-                        run = False
-                    if event.type == pygame.KEYDOWN:
-                        main()
                     
 
     
-main()
+menu(death_count)
